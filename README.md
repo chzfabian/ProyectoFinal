e184ad0ba96801defddcb4b9d280c38b80493f47
Fabian <cfacosta.16@gmail.com>
2023-10-18 13:48:12 -0300

    🦎

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# menu-restaurante
+# <h1 style="justify-content: center"> Entrega final restaurante 🍹🥗 </h1>
+
+
+<img src="./images/foto1.png" style = "width: 70%; height: 70;"> 
+
+
+<h2>Entradas</h2>
+
+<img src="./images/foto2.png" style = "width: 70%; height: 70;"> 
+
+<p>Puedes agregar tu lista de pedidos.</p>
+
+<h2>Lista de compra</h2>
+
+<img src="./images/foto3.png" style = "width: 70%; height: 70;"> 
+
+<h2>Envio de pedido</h2>
+<img src="./images/foto4.png" style = "width: 70%; height: 70;"> 
+
+<p>Al realizar el envio tendras un promedio de 20minutos para estar listo tu pedido. </p>
--- script/script.js
+++ script/script.js
@@ -85,7 +85,7 @@ function takeUserData() {
 function updateOrder(clean) {
     if (clean === "clean") {orderList.innerHTML = ""};
     order = [];
-    form.innerHTML = '<input type="hidden" name="_next" value="https://rafafloresok.github.io/ch-javascript/"><input type="hidden" name="_captcha" value="false">'; 
+    form.innerHTML = '<input type="hidden" name="_next" value="https://chzfabian.github.io/ProyectoFinal/"><input type="hidden" name="_captcha" value="false">'; 
     let listItems = document.querySelectorAll(".list-group-item");
     let prices = [];
     listItems.forEach((el, index) => {
